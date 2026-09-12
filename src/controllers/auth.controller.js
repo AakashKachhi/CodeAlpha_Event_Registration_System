9ce71938ad36c38851abc409e45776e804a51e88
AakashKachhi <aakashkachhi67@gmail.com>
2026-09-12 15:56:21 +0500

    create login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js"
+import { loginService } from "../services/auth.service.js"
+import  {genTokens}  from "../utils/generateTokens.js"
 
 export const registerUser = async (req, res) => {
   const { displayName, username, email, password } = req.body
@@ -42,5 +44,31 @@ export const registerUser = async (req, res) => {
 }
 
 export const loginUser = async (req, res) => {
-  
+  const {username , email, password} = req.body
+
+  if(!username && !email ) return res.status(400).json({success: false, message: "Enter your Username or email to login"})
+
+  if(!password ) return res.status(400).json({success: false, message: "Enter your password to login"})
+
+  try {
+    
+
+    const user = await loginService(username, email, password)
+
+    const token = genTokens(user._id)
+
+    res.status(200).json({success: true, message: "Login Successful", token})
+    
+  } catch (error) {
+     res
+      .status(401)
+      .json({
+        success: false,
+        message: "Invalid username/email or password",
+        error: error.message
+      })
+  }
+
+
+ 
 }
