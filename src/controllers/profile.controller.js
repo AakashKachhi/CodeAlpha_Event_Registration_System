4e77107a647064066f49483ba886c1d73573e965
AakashKachhi <aakashkachhi67@gmail.com>
2026-09-12 15:57:07 +0500

    to get profile

---
+++ src/controllers/profile.controller.js
@@ -0,0 +1,17 @@
+import { fetchProfile } from "../services/profile.service.js"
+
+export const getProfile = async (req, res) => {
+    try {
+        const profile = await fetchProfile(req.user.userId)
+
+        if(!profile) return res.status(404).json({message: "Profile not found"})
+
+        res.status(200).json({
+            displayName: profile.displayName,
+            username: profile.username,
+            email: profile.email
+        })
+    } catch (error) {
+        return res.status(500).json({message: "Something went wrong"})
+    }
+}
